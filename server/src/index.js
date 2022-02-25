aca5bf80b0bf46973e5e07d03277d7e883cc1615
Dhruv Bhardwaj <11dhruv.b@gmail.com>
2022-02-25 14:01:17 +0530

    stores and category

--- server/src/index.js
+++ server/src/index.js
@@ -7,12 +7,16 @@ const connect = require("./config/db");
 // importing all controllers.
 const categoryController = require("./controllers/category.controller");
 const subcategoryController = require("./controllers/subCategory.controller");
+const storesController = require("./controllers/stores.controller");
+const optionController = require("./controllers/cat.controllers");
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/category",categoryController);
 app.use("/subCategory",subcategoryController);
+app.use("/stores", storesController);
+app.use("/options", optionController);
 
 
 // Turning on the server and connection with db.
